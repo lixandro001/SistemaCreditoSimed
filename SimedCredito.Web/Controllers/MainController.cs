369297ff41e5dc9abc5ba77837c991685097b702
lixandro001 <gomezrinconlixandro@gmail.com>
2021-11-22 21:19:32 -0500

    //implemnatacion de fronedn y swagger

---
+++ SimedCredito.Web/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Simed.Utilities.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimedCredito.Web.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly ILogger Logger;
+        private readonly IHostingEnvironment HostingEnvironment;
+
+        public MainController(IConfiguration IConfiguration, ILoggerFactory LoggerFactory, IHostingEnvironment IHostingEnvironment)
+        {
+            HostingEnvironment = IHostingEnvironment;
+            Configuration = IConfiguration;
+            Logger = LoggerFactory.CreateLogger<MainController>();
+            GeneralModel.UrlWebApi = Configuration["Urls:UrlWebApi"];
+        }
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
